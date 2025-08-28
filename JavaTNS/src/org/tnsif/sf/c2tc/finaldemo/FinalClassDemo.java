07d6d8cf059966c4cfe4bd0b3a21aa735233563e
theja <theja@DESKTOP-V5N6MTA>
2025-08-28 22:02:18 +0530

    17 commit

---
+++ JavaTNS/src/org/tnsif/sf/c2tc/finaldemo/FinalClassDemo.java
@@ -0,0 +1,23 @@
+package org.tnsif.sf.c2tc.finaldemo;
+//Government ID generator
+
+
+final class AadhaarGenerator
+{
+	void generateID()
+	{
+		System.out.println("Generated Aadhaar card ");
+	}
+}
+
+
+public class FinalClassDemo {
+
+	public static void main(String[] args) {
+		
+		        AadhaarGenerator ag = new AadhaarGenerator();
+		        ag.generateID();
+		    
+	}
+
+}
---
+++ JavaTNS/src/org/tnsif/sf/c2tc/finaldemo/FinalMethod.java
@@ -0,0 +1,23 @@
+package org.tnsif.sf.c2tc.finaldemo;
+class EcommercePlatform
+{
+	final void calculateDiscount()
+	{
+		System.out.println("Base discount =10%");
+	}
+}
+class Amazon extends EcommercePlatform
+{
+	void calc()
+	{
+	    System.out.println("Extra Discount =20%");	
+	}
+}
+public class FinalMethod {
+
+	public static void main(String[] args) {
+		Amazon obj=new Amazon();
+		obj.calculateDiscount();
+
+	}
+}
